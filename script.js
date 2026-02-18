15e6543a39827646191fee9062c15a85fe7c034b
Shital Bagade <shitalbagade2005@gmail.com>
2026-02-18 11:45:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const text = ["Frontend Developer", "Web Designer", "BCA Student"];
+let i = 0;
+let j = 0;
+let currentText = "";
+let isDeleting = false;
+let speed = 100;
+
+function typeEffect() {
+    const typing = document.querySelector(".typing");
+    currentText = text[i];
+
+    if (!isDeleting) {
+        typing.textContent = currentText.substring(0, j++);
+        if (j > currentText.length) {
+            isDeleting = true;
+            setTimeout(typeEffect, 1000);
+            return;
+        }
+    } else {
+        typing.textContent = currentText.substring(0, j--);
+        if (j === 0) {
+            isDeleting = false;
+            i = (i + 1) % text.length;
+        }
+    }
+    setTimeout(typeEffect, speed);
+}
+
+typeEffect();
+
+function scrollToContact(){
+    document.getElementById("contact").scrollIntoView({behavior:"smooth"});
+}
 
